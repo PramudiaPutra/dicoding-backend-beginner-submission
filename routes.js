ae603448976a80cb65621952c04ee60daeebf6e8
pramudia_putra_pamungkas <pramudiaputra.iv@gmail.com>
2022-09-04 20:52:51 +0700

    using hapi and routing

---
+++ routes.js
@@ -0,0 +1,18 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: (req, h) => {
+            return 'Homepage'
+        }
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (req, h) => {
+            return 'About page'
+        }
+    },
+]
+
+module.exports = routes;

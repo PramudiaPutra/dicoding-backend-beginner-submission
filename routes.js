fb81039c22ea7ef9f985819a2c11a8500d955f7c
pramudia_putra_pamungkas <pramudiaputra.iv@gmail.com>
2022-09-04 21:06:03 +0700

    installing nodemon and eslint

--- routes.js
+++ routes.js
@@ -1,18 +1,18 @@
 const routes = [
-    {
-        method: 'GET',
-        path: '/',
-        handler: (req, h) => {
-            return 'Homepage'
-        }
+  {
+    method: 'GET',
+    path: '/',
+    handler: (req, h) => {
+      return 'Homepage';
     },
-    {
-        method: 'GET',
-        path: '/about',
-        handler: (req, h) => {
-            return 'About page'
-        }
+  },
+  {
+    method: 'GET',
+    path: '/about',
+    handler: (req, h) => {
+      return 'About page';
     },
-]
+  },
+];
 
 module.exports = routes;
